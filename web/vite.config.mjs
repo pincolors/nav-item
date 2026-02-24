186961812f91a49815b7ea961111a63db6bb8494
pincolors <121207009+pincolors@users.noreply.github.com>
2026-02-24 13:04:16 +0800

    Update vite.config.mjs

--- web/vite.config.mjs
+++ web/vite.config.mjs
@@ -55,13 +55,14 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:8080',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:8080',
         changeOrigin: true
       }
     }
   }
 })
+
